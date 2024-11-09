65530fab03bb03fc5e4766a913e3d21bc882c04d
Atik2788 <ashanto653.2@gmail.com>
2024-11-09 07:58:16 +0600

    completing feature part

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -1,4 +1,7 @@
 import {feature} from '../Images'
+import {features} from '../Data/dummy';
+import { TiTick } from 'react-icons/ti';
+import '../Styles/feature.scss'
 
 
 const Feature = () => {
@@ -6,9 +9,25 @@ const Feature = () => {
     <div className="app__feature">
         <div className="feature__container">
             <div className="left__feature">
-                <img src={feature} alt="" />
+                <img src={feature} alt="" draggable={false} />
             </div>
-            <div className="right__feature"></div>
+            <div className="right__feature">
+              <div className="right__text">
+                <h1>Feature to help <br /> your team succeed</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione facilis maxime id et ea aliquid laborum distinctio non accusamus ipsam nesciunt doloremque ad alias tempora, vero molestias ipsum ab deleniti.</p>
+              </div>
+              <div className="feature__content">
+                {features.map((feature, index) =>(
+                  <div className="feature" key={index}>
+                    <TiTick/> 
+                    <div className="feature__text">
+                      <p>{feature}</p>
+                      <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio, tempore.</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
         </div>
     </div>
   )
